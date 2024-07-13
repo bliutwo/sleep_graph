b6acbd92ae8b5c4c1a0593a316225b2956d27f1e
bliutwo <bliutwo@cs.stanford.edu>
2024-07-12 22:37:10 -0700

    Unsure how masks work yet, but need to account for gaps...
    ...and/or missing data in general

--- graph_with_classes.py
+++ graph_with_classes.py
@@ -5,8 +5,36 @@ import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.ma as ma
 
 DATES_KEY = 'Date'
+
+def create_masked_array(numpyarray):
+  mask = []
+  for value in numpyarray:
+    if value == np.nan:
+      mask.append(0)
+    else:
+      mask.append(1)
+  return ma.masked_array(numpyarray, mask=mask)
+
+class DataGrapher:
+  def __init__(self, data):
+    self.data = data
+  def graph(self):  
+    dates = self.data[DATES_KEY]
+    xpoints = np.array(dates)
+    #xpoints = create_masked_array(xpoints)
+    for label, list_of_points in self.data.items():
+      if label != DATES_KEY:
+        ypoints = np.array(list_of_points)
+        #ypoints = create_masked_array(ypoints)
+        plt.plot(xpoints, ypoints, label=label)
+    plt.legend()
+    plt.ylabel("Time in Hours")
+    plt.title("Natalie's Sleep Over Time")
+    plt.xticks(rotation=45, ha="right")
+    plt.show()
 
 def main():
   parser = argparse.ArgumentParser(
@@ -17,17 +45,8 @@ def main():
   print(f"Filename: {args.filename}")
   csv_parser = CsvParser(args.filename)
   data = csv_parser.get_data()
-  dates = data[DATES_KEY]
-  xpoints = np.array(dates)
-  for label, list_of_points in data.items():
-    if label != DATES_KEY:
-      ypoints = np.array(list_of_points)
-      plt.plot(xpoints, ypoints, label=label)
-  plt.legend()
-  plt.ylabel("Time in Hours")
-  plt.title("Natalie's Sleep Over Time")
-  plt.xticks(rotation=45, ha="right")
-  plt.show()
+  data_grapher = DataGrapher(data)
+  data_grapher.graph()
 
 if __name__ == "__main__":
   sys.exit(main())
--- parse_csv.py
+++ parse_csv.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 import argparse
 import json
+import numpy
 import re
 import sys
 
@@ -35,30 +38,55 @@ class ColumnHandler:
     self.column_label_to_list_of_values[label].append(value)
   def _needs_conversion(self, list_of_values):
     for item in list_of_values:
-      if 'm' in item:
+      if 'm' in item or ':' in item:
         return True
     return False
+  def _convert_datetime(self, raw_time_string):
+    res = True
+    format_strings = [
+      '%H:%M',
+      '%I:%M %p',
+    ]
+    for format_str in format_strings:
+      try:
+        datetime_obj = datetime.strptime(raw_time_string, format_str)
+        return datetime_obj.hour - 12, datetime_obj.minute
+      except Exception as e:
+        print(f"Ran into the exception: {e}")
+        continue
+    print(f"Tried to convert '{raw_time_string}' but failed")
+    return None
   def _convert_list(self, list_of_values):
     final_list_of_values = []
     list_of_values = [item.replace('min', 'm') for item in list_of_values]
     for raw_time_string in list_of_values:
-      x = re.search("(\d+)?h?(\d+)?m?", raw_time_string)
       final_value = 0
-      if x.group(1) is None or x.group(2) is None:
-        # If they are both none, do nothing
-        if x.group(1) is None and x.group(2) is None:
-          continue
-        # One of h or m is missing
-        if 'h' in raw_time_string:
-          # We're only looking at hour times
-          final_value = int(x.group(1)) * 60
+      if ':' not in raw_time_string:
+        #if raw_time_string != "0":
+        x = re.search("(\d+)?h?(\d+)?m?", raw_time_string)
+        if x.group(1) is None or x.group(2) is None:
+          # If they are both none, do nothing
+          if x.group(1) is None and x.group(2) is None:
+            continue
+          # One of h or m is missing
+          if 'h' in raw_time_string:
+            # We're only looking at hour times
+            final_value = int(x.group(1)) * 60
+          else:
+            # We're only looking at minute times
+            final_value = int(x.group(1))
         else:
-          # We're only looking at minute times
-          final_value = int(x.group(1))
+          hours = int(x.group(1))
+          minutes = int(x.group(2))
+          final_value = (hours * 60) + minutes
+        #else:
+        #  final_value = numpy.nan
       else:
-        hours = int(x.group(1))
-        minutes = int(x.group(2))
-        final_value = (hours * 60) + minutes
+        if 'quiet time' not in raw_time_string.lower():
+          hours, minutes = self._convert_datetime(raw_time_string)
+          final_value = (hours * 60) + minutes
+        else:
+          final_value = 0
       final_list_of_values.append(final_value)
     final_list_of_values = [minutes / 60.0 for minutes in final_list_of_values]
     return final_list_of_values
@@ -80,7 +108,7 @@ def main():
   args = parser.parse_args()
   print(f"Filename: {args.filename}")
   csv_parser = CsvParser(args.filename)
-  csv_parser.get_data()
+  print(json.dumps(csv_parser.get_data(), indent=2))
 
 if __name__ == "__main__":
   sys.exit(main())
